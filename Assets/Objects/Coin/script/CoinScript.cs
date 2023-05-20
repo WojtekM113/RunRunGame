576cdf1594ed4310841bc4ba57aefbc097b54955
Wm <wojtek.mendlik@gmail.com>
2023-05-20 15:15:37 +0200

    Commit

--- Assets/Objects/Coin/script/CoinScript.cs
+++ Assets/Objects/Coin/script/CoinScript.cs
@@ -5,45 +5,29 @@ using UnityEngine;
 
 public class CoinScript : MonoBehaviour, ITrigger {
 
-    
+
     float blendTreeParameter = 0;
 
 
     public CoinsListScriptableObj coinsListScriptableObj;
     public void TriggerObject() {
         Debug.Log("CollectedCoin");
-        Destroy(this.gameObject);
         coinsListScriptableObj.Coins.Add(1);
-        DOTween.Kill(this.gameObject);
-
+        DOTween.Kill(transform);
+        Destroy(this.gameObject);
     }
 
-    void Start()
-    {
+    void Start() {
         //animation
         DOTween.To(() => blendTreeParameter, (x) => blendTreeParameter = x, .5f, .5f);
 
-        if(transform.parent != null)
-        {
 
-           transform.DORotate(new Vector3(0, 360, 0), 2f, RotateMode.FastBeyond360)
-         .SetLoops(-1, LoopType.Restart)
-         .SetRelative()
-         .SetEase(Ease.Linear);
+        transform.DORotate(new Vector3(0, 360, 0), 2f, RotateMode.FastBeyond360)
+      .SetLoops(-1, LoopType.Restart)
+      .SetRelative()
+      .SetEase(Ease.Linear);
 
-        
-        }
-        
-            
-            coinsListScriptableObj.Coins.Clear();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-       
+        coinsListScriptableObj.Coins.Clear();
+  
     }
 }
--- Assets/Scripts/jumpPlatform/jumplPlatformScript.cs
+++ Assets/Scripts/jumpPlatform/jumplPlatformScript.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +7,24 @@ public class jumplPlatformScript : MonoBehaviour, ITrigger   {
 
     public PlayerAddForceScriptableObject playerAddForce;
     public Rigidbody playerRigidBody;
+    public GameObject player;
 
+    private float jumpFallSpeed = 1f;
 
- 
     public void TriggerObject() {
-        playerRigidBody.velocity = (Vector3.up * playerAddForce.addForce);
+       // player.transform.DOJump(new Vector3(5, 5, 5),5,1,jumpFallSpeed,true);
+        playerRigidBody.DOJump(new Vector3(5,5,0),5,1,jumpFallSpeed,true) ;
+        //playerRigidBody.velocity = (Vector3.up * playerAddForce.addForce);
     }
-
+    
     void Awake() {
  
-       playerRigidBody = playerRigidBody.GetComponent<Rigidbody>();
+      playerRigidBody = playerRigidBody.GetComponent<Rigidbody>();
 
     }
  
      
-   
+    
     
  
 }
